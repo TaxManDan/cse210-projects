2dab033b1c9eb8370904d0fffad81e9a824245aa
Danny Tolman <tol21004@byui.edu>
2024-01-09 13:03:21 -0700

    Completed Prep 1 and 2 Programs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your Grade Percentage?");
+        int gp = int.Parse(Console.ReadLine());
+        char letter;
+        if (gp >= 90){
+            letter = 'A';
+        }
+        else if (gp >= 80){
+           letter = 'B';
+        }
+        else if (gp >= 70){
+            letter = 'C';
+        }
+        else if (gp >= 60){
+            letter = 'D';
+        }
+        else {
+            letter = 'F';
+        }
+        Console.WriteLine($"You received the grade letter: {letter}.");
+        if (gp >= 70){
+            Console.WriteLine("Congrats on passing the class!");
+        }
+        else {
+            Console.WriteLine("Try again Next time.");
+        }
     }
 }
