202da8c2af53803182dfdbb62be5d25547121f16
Danny Tolman <tol21004@byui.edu>
2024-02-22 09:40:09 -0700

    Completed Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        string name1 = "Samuel Bennett";
+        string topic1 = "Multiplication";
+        var student1 = new Assignment(name1, topic1);
+        Console.WriteLine(student1.GetSummary());
+        string name2 = "Roberto Rodriguez";
+        string topic2 = "Fractions";
+        string textbookSection1 = "Section 7.3";
+        string problems1 = "8-19";
+        var student2 = new MathAssignment(name2, topic2, textbookSection1, problems1);
+        Console.WriteLine(student2.GetHomeworkList());
+        string name3 = "Mary Waters";
+        string topic3 = "European History";
+        string title1 = "The Causes of World War II";
+        var student3 = new WritingAssignment(name3, topic3, title1);
+        Console.WriteLine(student3.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,14 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string name, string topic, string title) : base(name, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation(){
+        string studentInfo = base.GetSummary();
+        return studentInfo + "\n" + _title + "by " + base.GetStudentName();
+    }
+}
