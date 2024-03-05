95a214b102bc5655e16eae143eebcc2f9b7ce17a
Danny Tolman <tol21004@byui.edu>
2024-03-05 12:41:03 -0700

    Created Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        var square1 = new Square(5);
+        square1.SetColor("Red");
+        shapes.Add(square1);
+        var rectangle1 = new Rectangle(3, 4);
+        rectangle1.SetColor("Blue");
+        shapes.Add(rectangle1);
+        var circle1 = new Circle(6);
+        circle1.SetColor("Green");
+        shapes.Add(circle1);
+
+        foreach (Shape shape in shapes){
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}");
+        }
+
     }
 }
